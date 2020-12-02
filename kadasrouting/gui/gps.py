7d4b34938a847e31881b2faf64a9b96708149cf5
Andrea Borghi <andrea.borghi@camptocamp.com>
2020-12-02 16:48:37 +0100

    use standard QGIS GPS registry

--- kadasrouting/gui/gps.py
+++ kadasrouting/gui/gps.py
@@ -20,55 +20,9 @@ LOG = logging.getLogger(__name__)
 
 @waitcursor
 def getGpsConnection():
-    gpsConnection = None
-    port = str(QgsSettings().value("/kadas/gps_port", ""))
-    gpsDetector = QgsGpsDetector(port)
-    loop = QEventLoop()
-
-    def gpsDetected(connection):
-        gpsConnection = connection  # NOQA
-        loop.exit()
-
-    def gpsDetectionFailed():
-        loop.exit()
-
-    gpsDetector.detected.connect(gpsDetected)
-    gpsDetector.detectionFailed.connect(gpsDetectionFailed)
-    gpsDetector.advance()
-    loop.exec_()
-
-    return gpsConnection
-
-
-def getMockupGpsConnection():
-    return GpsConnection(iface.mapCanvas())
-
-
-class GpsConnection(QObject):
-
-    statusChanged = pyqtSignal(object)
-
-    def __init__(self, canvas):
-        QObject.__init__(self)
-        self._canvas = canvas
-        # Update the point when the canvas extent changed.
-        self._canvas.extentsChanged.connect(self.currentLocationChanged)
-
-    def currentGPSInformation(self):
-        center = self._canvas.center()
-        outCrs = QgsCoordinateReferenceSystem(4326)
-        canvasCrs = self._canvas.mapSettings().destinationCrs()
-        transform = QgsCoordinateTransform(canvasCrs, outCrs, QgsProject.instance())
-        wgspoint = transform.transform(center)
-        info = QgsGpsInformation()
-
-        info.speed = 25
-        info.latitude = wgspoint.y()
-        info.longitude = wgspoint.x()
-        info.direction = 45
-        info.elevation = 200
-
-        return info
-
-    def currentLocationChanged(self):
-        self.statusChanged.emit(self.currentGPSInformation())
+    gpsConnectionList = QgsApplication.gpsConnectionRegistry().connectionList()
+    LOG.debug('gpsConnectionList = {}'.format(gpsConnectionList))
+    if len(gpsConnectionList) > 0:
+        return gpsConnectionList[0]
+    else:
+        return None
